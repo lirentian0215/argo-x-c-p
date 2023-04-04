51611382d35e0bff266f507eb3182a7878ca2c13
lirentian0215 <83382249+lirentian0215@users.noreply.github.com>
2023-04-04 23:20:46 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 设置各变量
 WSPATH=${WSPATH:-'argo'}
-UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
+UUID=${UUID:-'cfa0002d-e1d7-4cf3-aef6-de86fdfd7e37'}
 
 generate_config() {
   cat > config.json << EOF
